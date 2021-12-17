924d15499dc728beef6629702d5773e5afdd4bff
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 16:37:16 +0300

    Scripted auto-commit on change (2021-12-17 16:37:16) by gitwatch.sh

--- src/main/java/posttest/TesterActor.java
+++ src/main/java/posttest/TesterActor.java
@@ -3,7 +3,7 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.japi.pf.ReceiveBuilder;
 
-
+import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
