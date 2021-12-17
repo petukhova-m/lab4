24567a4aebb38414f9307024e1513ccc958d00f7
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 17:02:21 +0300

    Scripted auto-commit on change (2021-12-17 17:02:21) by gitwatch.sh

--- src/main/java/posttest/PostTestApp.java
+++ src/main/java/posttest/PostTestApp.java
@@ -15,7 +15,7 @@ import akka.http.javadsl.server.Route;
 import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
-
+import scala.concurrent.Future;
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
