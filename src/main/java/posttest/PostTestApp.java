5199f01af1a986eada5794eee83f735c8a46c1da
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 16:05:31 +0300

    Scripted auto-commit on change (2021-12-17 16:05:31) by gitwatch.sh

--- src/main/java/posttest/PostTestApp.java
+++ src/main/java/posttest/PostTestApp.java
@@ -9,7 +9,7 @@ import akka.http.javadsl.IncomingConnection;
 import akka.http.javadsl.ServerBinding;
 
 import akka.http.javadsl.model.HttpRequest;
-
+import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 
