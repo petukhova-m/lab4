e8289b381bc1bb4a83ae8d6b80d807e2537011be
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 16:52:40 +0300

    Scripted auto-commit on change (2021-12-17 16:52:40) by gitwatch.sh

--- src/main/java/posttest/TestResult.java
+++ src/main/java/posttest/TestResult.java
@@ -18,7 +18,7 @@ public class TestResult {
     }
 
     @Override
-
+    public String toString() {
         return "posttest.TestResult{" +
                 "packageId=" + packageId +
                 ", result='" + result + "\'" +
