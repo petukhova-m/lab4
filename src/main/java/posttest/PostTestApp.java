1b2279191eabf7021d3131b1e7886e591753eef9
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 17:09:02 +0300

    Scripted auto-commit on change (2021-12-17 17:09:02) by gitwatch.sh

--- src/main/java/posttest/PostTestApp.java
+++ src/main/java/posttest/PostTestApp.java
@@ -35,7 +35,7 @@ public class PostTestApp extends AllDirectives {
                 ConnectHttp.toHost("localhost", serverport),
                 materializer
         );
-
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
         System.in.read();
         binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
     }
