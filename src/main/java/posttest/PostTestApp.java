20975221cc5783317dfda86d8261c4a2da26319c
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 16:05:01 +0300

    Scripted auto-commit on change (2021-12-17 16:05:01) by gitwatch.sh

--- src/main/java/posttest/PostTestApp.java
+++ src/main/java/posttest/PostTestApp.java
@@ -30,7 +30,7 @@ public class PostTestApp extends AllDirectives {
         PostTestApp instance = new PostTestApp();
 
         routeFlow = instance.createRoute(system).flow(system, materializer);
-
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
 
 
 
