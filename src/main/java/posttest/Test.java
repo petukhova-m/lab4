bf6e977068939a4414833ed281bd1695591bcbe6
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 15:37:08 +0300

    Scripted auto-commit on change (2021-12-17 15:37:08) by gitwatch.sh

--- src/main/java/posttest/Test.java
+++ src/main/java/posttest/Test.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.*;
 import java.util.ArrayList;
 
 
-
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
         "testName",
         "expectedResult",
