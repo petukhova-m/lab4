931f309e89854e2fccbf3befc50672504cfe2fd0
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 16:59:33 +0300

    Scripted auto-commit on change (2021-12-17 16:59:33) by gitwatch.sh

--- src/main/java/posttest/Test.java
+++ src/main/java/posttest/Test.java
@@ -43,7 +43,7 @@ public class Test {
     @JsonProperty("expectedResult")
     public void setExpectedResult(String expectedResult) {
         this.expectedResult = expectedResult;
-    }
+
 
     @JsonProperty("params")
     public void setParams(List<String> params) {
