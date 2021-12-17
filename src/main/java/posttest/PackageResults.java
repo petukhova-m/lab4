9b1587df5d861a0544fdf41f591242fd9e3f06b5
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 16:56:06 +0300

    Scripted auto-commit on change (2021-12-17 16:56:06) by gitwatch.sh

--- src/main/java/posttest/PackageResults.java
+++ src/main/java/posttest/PackageResults.java
@@ -1,9 +1,9 @@
-package posttest;
+
 
 
 public class PackageResults {
     private ArrayList<String> results;
-
+    private int packageId;
 
     public ArrayList<String> getResults() {
         return results;
--- src/main/java/posttest/PackageResultsRequest.java
+++ src/main/java/posttest/PackageResultsRequest.java
@@ -8,6 +8,6 @@ package posttest;
     }
 
 
-
+        return packageId;
     }
 }
