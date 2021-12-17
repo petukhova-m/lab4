8238dc9bc471235d0fd0c9c4b2b0996bc30e26a5
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 17:03:21 +0300

    Scripted auto-commit on change (2021-12-17 17:03:21) by gitwatch.sh

--- src/main/java/posttest/Test.java
+++ src/main/java/posttest/Test.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class Test {
     @JsonProperty("testName")
-
+    private String testName;
     @JsonProperty("expectedResult")
     private String expectedResult;
 
